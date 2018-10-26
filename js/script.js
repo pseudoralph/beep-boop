3359600908029cfac56962479dc868f30fe68dcb
Ralph Perdomo <ralph@prdmo.com>
2018-10-26 09:16:11 -0700

    add simple iterator and output expected results to console

--- js/script.js
+++ js/script.js
@@ -11,26 +11,29 @@ function daveMsg() {
   console.log("I'm sorry, Dave. I'm afraid I can't do that.")
 }
 
-function responseRouter(num) {
-
-  if (num !=0 && num % 3 === 0) {
-    daveMsg();
-    return;
-  }
-  if (num.toString().indexOf("1") > -1) {
-    boop();
-    return;
-  }
-  if (num.toString().indexOf("0") > -1) {
-    beep();
-    return;
+function responseRouter(userEntered) {
+  for (var num=0; num < userEntered; num ++){
+    if (num !=0 && num % 3 === 0) {
+      daveMsg();
+      continue;
+    }
+    if (num.toString().indexOf("1") > -1) {
+      boop();
+      continue;
+    }
+    if (num.toString().indexOf("0") > -1) {
+      beep();
+      continue;
+    } else {
+      console.log(num)
+    }
   }
 
 }
 
 function main() {
 
-  responseRouter(0)
+  responseRouter(30)
 
 
 
